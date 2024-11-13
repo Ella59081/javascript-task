2e52e32eb4499c61c9de49f440e7e867126d52a7
Ella59081 <elea123nor@gmail.com>
2024-11-13 19:11:43 +0100

    message

---
+++ index.js
@@ -0,0 +1,26 @@
+var pie = 22;
+var pie2 = 7;
+var radius = 7;
+var answer = pie / pie2 * radius * radius;
+console.log(answer);
+
+let half = 0.5;
+let base = 12;
+let height = 8;
+let area = half * base * height;
+console.log(area);
+
+let principal = 1000;
+let rate = 5;
+let time = 3;
+let percent = 100;
+let interest = principal * rate * time / percent;
+console.log(interest);
+
+let a = 3;
+let b = 12;
+let c = 5;
+let problem = - b + Math.sqrt( b * b - 4 * a * c ) / 2 * a;
+console.log(problem);
+let solution = - b - Math.sqrt ( b * b - 4 * a * c) / 2 * a;
+console.log(solution);
